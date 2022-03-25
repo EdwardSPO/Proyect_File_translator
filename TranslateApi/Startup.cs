31268842ae39c32ca2cf3dbd0412d07f6edfab73
Sago21 <jsgaravito90@misena.edu.co>
2022-03-25 07:53:22 -0500

    Feature: 103411 store file from API

--- TranslateApi/Startup.cs
+++ TranslateApi/Startup.cs
@@ -11,7 +11,7 @@ using System;
 using System.Collections.Generic;  
 using System.Linq;  
 using System.Threading.Tasks;  
-using TranslateApi.Services;  
+using  TranslateApi.Services;  
   
 namespace TranslateApi
 {  
