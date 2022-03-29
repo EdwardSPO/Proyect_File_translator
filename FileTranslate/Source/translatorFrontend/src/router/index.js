825ad91f004110a560d1847b13811260d2ebefd1
Sago21 <jsgaravito90@misena.edu.co>
2022-03-29 18:25:07 -0500

    Feature: 103411 store file from frontend and validation

---
+++ FileTranslate/Source/translatorFrontend/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Dashboard from '../views/Dashboard.vue'
+import Editar from '../views/Editar.vue'
+import Nuevo from '../views/Nuevo.vue'
+import Translator from '../views/Translator'
+
+
+
+
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Translator',
+        component: Translator
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/editar/:id',
+        name: 'Editar',
+        component: Editar
+    },
+    {
+        path: '/nuevo',
+        name: 'Nuevo',
+        component: Nuevo
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ FileTranslate/Source/translatorFrontend/src/views/Translator.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <h1>Traductor</h1>
+    <div class="header"></div>
+
+    <div class="container">
+      <div class="mb-3">
+        <input
+          class="form-control"
+          type="file"
+          id="formFile"
+          v-on:change="getFile($event)"
+        />
+      </div>
+
+      <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupSelect01">Idioma</label>
+        <select class="form-select" id="inputGroupSelect01">
+          <option selected>Seleccione el idioma</option>
+          <option value="1"></option>
+          <option value="2"></option>
+          <option value="3"></option>
+        </select>
+      </div>
+      -->
+      <br />
+      <div class="progress">
+        <p>
+          Progress:
+          <!-- <progress :value="uploadValue" max='100'></progress> -->
+        </p>
+      </div>
+
+      <div
+        class="btn-group"
+        role="group"
+        aria-label="Basic mixed styles example"
+      >
+        <button
+          id="traduccion"
+          type="button"
+          class="btn btn-success"
+          @click="submitForm($event)"
+        >
+          Traducir
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+const url = "https://localhost:5001/api/File/Upload?";
+
+
+// function handleFiles(files) {
+//   var d = document.getElementById("fileList");
+//   if (!files.length) {
+//     d.innerHTML = "<p>¡No se han seleccionado archivos!</p>";
+//   } else {
+//     var list = document.createElement("ul");
+//     d.appendChild(list);
+//     for (var i=0; i < files.length; i++) {
+//       var li = document.createElement("li");
+//       list.appendChild(li);
+
+//       var img = document.createElement("img");
+//       img.src = window.createBlobURL(files[i]);;
+//       img.height = 60;
+//       img.onload = function() {
+//         window.revokeBlobURL(this.src);
+//       }
+//       li.appendChild(img);
+
+//       var info = document.createElement("span");
+//       info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+//       li.appendChild(info);
+//     }
+//   }
+// }
+
+
+export default {
+  data() {
+    return {
+      uploadURL: url + "subDirectory=File",
+      file: "",
+    };
+  },
+  methods: {
+    getFile(event) {
+    var formFile = document.getElementById("formFile");
+    var archivoRuta = formFile.value;
+    var extPermitidas = /(.docx|.txt)$/i;
+
+    if(!extPermitidas.exec(archivoRuta)){
+alert('Asegurese de haber seleccionado un archivo tipo docx o txt')
+formFile.value=''
+return false; 
+    }
+   
+    
+      var ValidationFile= document.getElementById("formFile").files[0].size;
+       if(ValidationFile > 500000000){
+      alert("El archivo no debe pesar mas de 500MB")
+    } else{
+      this.file = event.target.files[0];
+      console.log(this.file);
+    }
+    },
+    submitForm(event) {
+      event.preventDefault();
+      let formData = new FormData();
+      formData.append("file", this.file);
+
+      let config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      this.$http
+        .post(this.uploadURL,formData,config)
+        .then(function (response) {
+          if (response.status === 200) {
+            console.log(response.data);
+          }
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+.header {
+  position: absolute;
+  background-color: rgb(13, 70, 51);
+  width: 2365px;
+  height: 65px;
+  top: -15px;
+}
+
+h1 {
+  position: absolute;
+  color: darkgreen;
+  top: 80px;
+  left: 800px;
+}
+
+.form-control {
+  position: absolute;
+  top: 250px;
+  left: 320px;
+  width: 1365px;
+  height: 40px;
+}
+
+.form-select {
+  position: absolute;
+  top: 300px;
+  left: 80px;
+  width: 100px;
+  height: 40px;
+}
+
+.input-group-text {
+  position: absolute;
+  top: 300px;
+  left: 10px;
+  height: 40px;
+}
+
+.btn {
+  position: absolute;
+  top: 350px;
+  left: 650px;
+}
+.progress {
+  position: absolute;
+  top: 460px;
+  left: 1300px;
+}
+</style>
