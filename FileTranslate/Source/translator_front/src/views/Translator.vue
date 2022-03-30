9762448ca24892aff4966bbbbf75bd2a4e62682c
Sago21 <jsgaravito90@misena.edu.co>
2022-03-30 11:09:34 -0500

    Feature: 103041 store file from frontend and validation

--- FileTranslate/Source/translator_front/src/views/Translator.vue
+++ FileTranslate/Source/translator_front/src/views/Translator.vue
@@ -49,34 +49,8 @@
   </div>
 </template>
 <script>
-const url = "https://localhost:5001/api/File/Upload?";
+const url = "http://localhost:5000/api/File/Upload?";
 
-
-// function handleFiles(files) {
-//   var d = document.getElementById("fileList");
-//   if (!files.length) {
-//     d.innerHTML = "<p>¡No se han seleccionado archivos!</p>";
-//   } else {
-//     var list = document.createElement("ul");
-//     d.appendChild(list);
-//     for (var i=0; i < files.length; i++) {
-//       var li = document.createElement("li");
-//       list.appendChild(li);
-
-//       var img = document.createElement("img");
-//       img.src = window.createBlobURL(files[i]);;
-//       img.height = 60;
-//       img.onload = function() {
-//         window.revokeBlobURL(this.src);
-//       }
-//       li.appendChild(img);
-
-//       var info = document.createElement("span");
-//       info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
-//       li.appendChild(info);
-//     }
-//   }
-// }
 
 
 export default {
