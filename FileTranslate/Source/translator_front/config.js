d80354fdf135e8fa218565cb51d3ec4c842464a2
Sago21 <jsgaravito90@misena.edu.co>
2022-04-22 12:41:20 -0500

    advance translation frontend

---
+++ FileTranslate/Source/translator_front/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    API_USERS: "http://localhost:50750/api/user",
+    URL_FILE: "http://localhost:5000/download_file/606c0903-04e1-4211-8e1b-17670af8d128.holaa_en.txt",
+    url: "http://localhost:5000/download_file/1b575897-04d9-4a53-81a7-ae556c5345b2.holaa_en.txt"
+}
