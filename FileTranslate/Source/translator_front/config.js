e3dc96a7bd6fc4dd51d14fb444770b5bdaeef029
Sago21 <jsgaravito90@misena.edu.co>
2022-04-26 10:08:05 -0500

    advance translation frontend

--- FileTranslate/Source/translator_front/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    API_USERS: "http://localhost:50750/api/user",
-    API_LANGUAGES: "http://localhost:5000/languages",
-    url: "https://localhost:5024/api/File/Upload?subDirectory=file",
-    urlTranslate: "https://localhost:5024/api/File/UploadTranslate?source=ar&target=en"
-}
